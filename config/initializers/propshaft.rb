05c9b836f538d1ee3bb785039c2ed8f0e3763ec5
Dingu-suke <e2310000rubik@gmail.com>
2024-03-25 06:34:13 +0900

    🔧[add]:file_for_custom

---
+++ config/initializers/propshaft.rb
@@ -0,0 +1,6 @@
+# Rails.application.config.propshaft.loader = Propshaft::Loader.new(
+#   paths: Rails.application.config.assets.paths,
+#   prefix: Rails.application.config.assets.prefix
+# )
+
+# これは propsahft のカスタム設定ファイル
