5b2746f1a97522773ea800996faaf3b9e2bf4416
Dingu-suke <e2310000rubik@gmail.com>
2024-03-27 22:21:38 +0900

    🔧[update_custom_settings]propshaft.rb

--- config/initializers/propshaft.rb
+++ config/initializers/propshaft.rb
@@ -9,3 +9,14 @@
 #   config.propshaft.javascript_pipeline = :esbuild
 #   config.propshaft.webpack.exclude << 'app/javascript/**/*.js'
 # end
+
+Rails.application.config.assets.build_with = {
+  Propshaft::Esbuild => { inputs:  [
+                            Rails.root.join('app/javascript/**/*.js')
+                            Rails.root.join('app/javascript/*.js')
+                            ],
+                          options: {} },
+  Propshaft::Tailwindcss => { inputs: Rails.root.join('app/assets/stylesheets'),
+                          options: {}, }
+}
+# あとで調べる ちゃんとやる
