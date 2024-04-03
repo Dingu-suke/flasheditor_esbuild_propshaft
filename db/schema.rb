1ca7744a7b1a65eaf627eda0b068ca99f8f30cbe
Dingu-suke <e2310000rubik@gmail.com>
2024-04-04 04:03:01 +0900

    [draft_table]

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_31_070407) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_085814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "drafts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_drafts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
