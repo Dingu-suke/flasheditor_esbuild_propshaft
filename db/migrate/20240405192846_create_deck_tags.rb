f8f3c1a656a46ea34a063a571d898553191f5ae4
Dingu-suke <e2310000rubik@gmail.com>
2024-04-07 02:33:25 +0900

    [intermediate]create tables and define associations

---
+++ db/migrate/20240405192846_create_deck_tags.rb
@@ -0,0 +1,10 @@
+class CreateDeckTags < ActiveRecord::Migration[7.1]
+  def change
+    create_table :deck_tags do |t|
+      t.references :deck, null: false, foreign_key: true
+      t.references :tag, null: false, foreign_key: true
+      t.timestamps 
+    end
+    add_index :deck_tags [:deck_id, :card_id], unique: true
+  end
+end
