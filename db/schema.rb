21feac21df5ef4ef1f4dbde5b79ae7d939d5f962
Dingu-suke <e2310000rubik@gmail.com>
2024-04-07 02:34:09 +0900

    [create]models_and_tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_194245) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_05_161407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +19,21 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_194245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_decks_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_194245) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
+  add_foreign_key "decks", "users"
 end
