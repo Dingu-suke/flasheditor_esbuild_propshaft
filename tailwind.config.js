fc66128019784e8caf7d5972bef40aca24ad69a2
Dingu-suke <e2310000rubik@gmail.com>
2024-04-07 22:40:31 +0900

    [create]menu_bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,15 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   plugins: [require("daisyui")],
+  theme: {
+    fontsize: {
+      'xs': '0.75rem', // 12px
+      'sm': '0.875rem', // 14px
+      'base': '1rem', // 16px
+      'lg': '1.125rem', // 18px
+      'xl': '1.25rem', // 20px
+      '2xl': '1.5rem', // 24px
+      'small': '0.75rem', // 12px
+    }
+  }
 }
