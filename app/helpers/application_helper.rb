8ff84bde21ff3a6feb5ca7f70d30f9893300641c
Dingu-suke <e2310000rubik@gmail.com>
2024-04-09 22:15:29 +0900

    [helper]書いただけでまだ使わん

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def render_cards_helper(cards)
+    return 'カードはまだありません' if cards.blank?
+
+    cards.inject(''.html_safe) do |output, card|
+      output + content_tag(:div, class: "card w-64 bg-base-100 shadow-xl") do
+        content_tag(:div, class: "card-body") do
+          concat(content_tag(:h2, card.title, class: "card-title"))
+          concat(content_tag(:p, card.body))
+          concat(content_tag(:div, class: "card-actions justify-end") do
+            content_tag(:div, class: "form-control") do
+              label_tag('', class: 'label cursor-pointer') do
+                concat(content_tag(:span, '', class: 'label-text'))
+                concat(check_box_tag('', '', false, class: 'checkbox checkbox-primary'))
+              end
+            end
+          end)
+        end
+      end + tag.br
+    end
+  end
+
 end
