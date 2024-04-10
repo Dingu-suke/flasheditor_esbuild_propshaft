78f1052776a7490df948364f7df90895de16226b
Dingu-suke <e2310000rubik@gmail.com>
2024-04-11 03:29:00 +0900

    [fix]association_mistake

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,13 +2,13 @@ class Deck < ApplicationRecord
   belongs_to :user
   has_many :deck_cards
   has_many :cards, through: :deck_cards
-  has_many :deck_tags, dependent: :destroy
+  has_many :deck_tags
   has_many :tags, through: :deck_tags
 
   validates :name, presence: true, length: { maximum: 255 }
 
-  def at_least_five_cards
-    if cards.size < 1
+  def at_least_threee_cards
+    if cards.size < 3
       errors.add(:cards, 'は最低1枚選択してください')
     end
   end
