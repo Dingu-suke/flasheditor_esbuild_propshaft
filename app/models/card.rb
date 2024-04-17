47fc14d1ad155cbcbd3c278a43e30bbea978d6ca
Dingu-suke <e2310000rubik@gmail.com>
2024-04-17 14:53:09 +0900

    [validation]custom

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,13 @@ class Card < ApplicationRecord
   has_many :deck_cards
   has_many :decks, through: :deck_cards
   
+  before_validation :set_default_remarks, on: :create
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
+  validates :remarks, presence: true, length: { maximum: 65_535 }
+  
+  def set_default_remarks
+    self.remarks = 'なし' if body.blank?
+  end
 end
