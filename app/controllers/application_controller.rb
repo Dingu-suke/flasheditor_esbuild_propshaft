d033ee94ed0d5f1b617c1431121ec3c9a8243524
Dingu-suke <e2310000rubik@gmail.com>
2024-05-29 14:32:50 +0900

    [🐛]:他ユーザーが作成したオブジェクトを削除できるバグを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :store_current_location, unless: :devise_controller?
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
   def authenticate_user!
     unless user_signed_in?
@@ -7,6 +10,19 @@ class ApplicationController < ActionController::Base
   end
 
   def set_card
-    @card = Card.find(params[:id])
+    @card = current_user.cards.find(params[:id])
+  end
+
+  def store_current_location
+    session[:previous_url] = request.fullpath if request.get? && !request.xhr?
+  end
+
+  def render_404
+    # flash.now[:danger] = "データが見つかりません。"
+    # render :index, status: :not_found
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: "データが見つかりませんでした。"}
+      format.any { head :not_found }
+    end
   end
 end
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,8 @@
 class CardsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_card, only: [:show, :edit, :update, :destroy]
+  before_action :set_card, only: [:show, :edit, :update, :destroy_your_card]
+  before_action :authorize_user!, only: [:show, :edit, :update, :destroy_your_card]
+
   def new
     @card = Card.new()
     @card.body = "```ruby\n#ここにコメントアウトで問題文を書く\n#ここにコードを書く\n```"
@@ -8,16 +10,14 @@ class CardsController < ApplicationController
   end
 
   def index
-    @drafts = Card.includes(:user)
-    # pborads = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
+    # @drafts = Card.includes(:user)
+    # @borads = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def show
-    @card = Card.find(params[:id])
   end
 
   def edit
-    @card = Card.find(params[:id])
   end
 
   def create
@@ -29,36 +29,46 @@ class CardsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-
+  
   def update
     @card = current_user.cards.find(params[:id])
     if @card.update(cards_params)
       redirect_to your_cards_path, success: "保存成功"
     else
       flash.now[:danger] = "保存失敗"
-      render :new, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
-
-  def destroy
-    @card.destroy!
-    redirect_to your_cards_path, success: "カードを削除しました", status: :see_other
-  end
-
+  
+  # def destroy
+  #   @card.destroy!
+  #   redirect_to your_cards_path, success: "カードを削除しました", status: :see_other
+  # end
+  
   def your_cards 
     @your_cards = Card.where(user_id: current_user.id).includes(:user).order("created_at DESC")
   end
   
   def destroy_your_card
-    @card = Card.find(params[:id])
     @card.deck_cards.destroy_all
     @card.destroy!
     redirect_to your_cards_path, success: "カードを削除しました", status: :see_other
   end
 
   private
+
+
+  def authorize_user!
+    unless @card.user_id == current_user.id
+      respond_to do |format|
+        format.html { redirect_to root_path, alert: "あなたはこの操作を行う権限がありません。" }
+        format.js { render 'shared/unauthorized', status: :forbidden }
+      end
+    end
+  end
+
   def cards_params
     params.require(:card).permit(:title, :body, :answer, :remarks)
   end
-
+  
 end
